5a06a1ccaa0a72ffc6d4c13b7c58646838ad2811
carlosvmelo <carlosvmelo@9b3b2ac9-4eca-fa1c-0e99-ceeafb79ce87>
2012-01-26 13:32:27 +0000

    Atenção!!!, fiz algumas alterações e o banco não vai funionar a não ser que vc coloque uma cópia do banco na pasta: C:\Users\Nome_do_usuario\Documents\banco, depois explico pessoalmenteo motivo

--- Cronos/src/senai/cronos/Main.java
+++ Cronos/src/senai/cronos/Main.java
@@ -1,6 +1,9 @@
 package senai.cronos;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -19,13 +22,81 @@ import senai.cronos.util.Contador;
  * @author sergio lisan e carlos melo
  */
 public class Main {
+    
+public static final String readRegistry(String location, String key)
+{
+  try
+  {
+      // Run reg query, then read output with StreamReader (internal class)
+      Process process = Runtime.getRuntime().exec("reg query " + 
+              '"'+ location + "\" /v " + key);
 
+      InputStream is = process.getInputStream();
+      StringBuilder sw = new StringBuilder();
+
+      try
+      {
+         int c;
+         while ((c = is.read()) != -1)
+             sw.append((char)c);
+      }
+      catch (IOException e)
+      { 
+      }
+
+      String output = sw.toString();
+
+      // Output has the following format:
+      // \n<Version information>\n\n<key>    <registry type>    <value>\r\n\r\n
+      int i = output.indexOf("REG_SZ");
+      if (i == -1)
+      {
+          return null;
+      }
+
+      sw = new StringBuilder();
+      i += 6; // skip REG_SZ
+
+      // skip spaces or tabs
+      for (;;)
+      {
+         if (i > output.length())
+             break;
+         char c = output.charAt(i);
+         if (c != ' ' && c != '\t')
+             break;
+         ++i;
+      }
+
+      // take everything until end of line
+      for (;;)
+      {
+         if (i > output.length())
+             break;
+         char c = output.charAt(i);
+         if (c == '\r' || c == '\n')
+             break;
+         sw.append(c);
+         ++i;
+      }
+
+      return sw.toString();
+  }
+  catch (Exception e)
+  {
+      return null;
+  }
+}
     public static void main(String[] args) {
         Main m = new Main();
 
         try {
-            String location = System.getProperty("user.home")+"\\Documents\\banco";
-            System.out.println(location);
+            //String location = System.getProperty("user.home")+"\\Documents\\banco";
+            
+           
+             String location = m.readRegistry("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\" 
+                 + "Explorer\\Shell Folders", "Personal")+"\\banco";
+        System.out.println(location);
             System.setProperty("derby.system.home", location);
             NetworkServerControlImpl networkServer = new NetworkServerControlImpl();
             networkServer.start(new PrintWriter(System.out));
