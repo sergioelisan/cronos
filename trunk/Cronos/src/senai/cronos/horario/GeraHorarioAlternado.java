f3599bde660ae2519a94f3664db0343477a538f2
carlosvmelo <carlosvmelo@9b3b2ac9-4eca-fa1c-0e99-ceeafb79ce87>
2012-12-22 17:48:14 +0000

    Atenção aos itens modificados: 1.modificações na lógica de cadastro

--- trunk/Cronos/src/senai/cronos/horario/GeraHorarioAlternado.java
+++ trunk/Cronos/src/senai/cronos/horario/GeraHorarioAlternado.java
@@ -6,10 +6,12 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import senai.cronos.CronosAPI;
 import senai.cronos.entidades.Aula;
 import senai.cronos.entidades.Docente;
@@ -21,12 +23,24 @@ public class GeraHorarioAlternado extends GeraHorario {
 
     @Override
     public void alocarAulas(Map<Date, Tupla<Aula, Aula>> horario) throws ClassNotFoundException, SQLException {
-Set<Date> keySet = null;
+        Set<Date> keySet = null;
        for(int modulo=1;modulo<3;modulo++){
            if(keySet==null){
-              keySet=horario.keySet(); 
+              keySet=horario.keySet();
+           
+             
            }
-        int modo = 0;
+           for (Date dia : keySet) 
+                    System.out.println("modulo:"+modulo+"//dia:"+dia.toString());
+           int modo = 0;      
+           TreeSet ts=new TreeSet();
+           ts.addAll(keySet);
+           if(ts.first().toString().contains("fri")||ts.first().toString().contains("wed")||ts.first().toString().contains("mon")){
+                        modo=0;
+                    }else{
+                        modo=1;
+                    }
+       
         for (UnidadeCurricular uc : getDisciplinas(modulo)) {
             Aula aula = getAula(uc);
             int total = getQuantidadeDeDias(uc, GeraHorario.TURNO_INTEIRO);
@@ -34,6 +48,7 @@ Set<Date> keySet = null;
             for (int i = 0; i < total; i++) {
                 OUTER:
                 for (Date dia : keySet) {
+                   
                     String diaSemana = DateUtil.getNomeDia(dia);
                     if (modo == 0) {
                         switch (diaSemana) {
@@ -66,12 +81,12 @@ Set<Date> keySet = null;
 
             modo = (modo == 0) ? 1 : 0;
         }
-        if(modulo<2) keySet=limparHorario(horario,modulo);
+        if(modulo<3) keySet=limparHorario(horario,modulo);
        }
     }
 
     public Set<Date> limparHorario(Map<Date, Tupla<Aula, Aula>> horario, int t) {
-        Set<Date> set=new HashSet();
+        Set<Date> set=new LinkedHashSet();
         Date[] datas = new Date[500];
         Map<Date, Tupla<Aula, Aula>> temp = new TreeMap<>();
         temp=Horario.create();
@@ -97,7 +112,7 @@ Set<Date> keySet = null;
        for (Date dia2 : horario.keySet()) {
            
            if (horario.get(dia2).getPrimeiro().equals(Aula.PADRAO)) {
-               System.out.println(dia2.toString()); 
+              // System.out.println(dia2.toString()); 
                set.add(dia2);
             }
         }
